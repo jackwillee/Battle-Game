a975f2c1f6977cc95b359e89bad4e16e34a4f971
your name <jackwillee1995@gmail.com>
2020-05-17 01:19:05 +0200

    created a web helper to assist with writing feature tests

--- spec/feature/entering_players_spec.rb
+++
@@ -1,9 +0,0 @@
-feature 'Enter names' do
-    scenario 'submitting names' do
-      visit('/')
-      fill_in :player_1_name, with: 'Jack'
-      fill_in :player_2_name, with: 'John'
-      click_button 'Submit'
-      expect(page).to have_content 'Jack vs. John'
-    end
-  end
